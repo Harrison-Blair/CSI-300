9ec7e86ead80fd1ead65f4695be77b35007c8716
Blair <harrison.blair@mymail.champlain.edu>
2024-04-18 15:45:12 -0400

    views and trigger stuff

--- Employee Database/hi_lo_salary_test.sql
+++ Employee Database/hi_lo_salary_test.sql
@@ -8,8 +8,9 @@ lowsal = 35000
 hisal = 45000
 */
 
-select * from emphistory where eid = 163;
+select * from emphistory where eid > 162;
 
 insert into emphistory values(163, 6, NOW(), 3300, "Awesome Salary", 121212);
+insert into emphistory values(164, 6, NOW(), 1113300, "Awesome Another Salary", 121212);
 
-select * from emphistory where eid = 163;
+select * from emphistory where eid > 162;
--- Employee Database/hi_lo_salary_trigger.sql
+++ Employee Database/hi_lo_salary_trigger.sql
@@ -1,14 +1,14 @@
 use employees;
 
-drop trigger if exists SalAdj;
+drop trigger if exists SalAdjLo;
+drop trigger if exists SalAdjHi;
 
 delimiter //
-create trigger SalAdj before insert on emphistory
+create trigger SalAdjLo before insert on emphistory
 for each row begin
-	declare hisal int;
+	-- declare hisal int;
     declare lowsal int;
-    declare diff int;
-    
+/*
     select sal_value into hisal
     from salpos sp1 inner join (
     select pos_id, max(sal_date) as MaxDate
@@ -16,8 +16,7 @@ for each row begin
 	group by pos_id) sp2 on sp1.pos_id = sp2.pos_id and sp1.sal_date = sp2.MaxDate
     where new.pos_id = sp1.pos_id and
 			sal_id = 2;
-            
-
+*/
     select sal_value into lowsal
     from salpos sp1 inner join (
     select pos_id, max(sal_date) as MaxDate
@@ -25,19 +24,32 @@ for each row begin
 	group by pos_id) sp2 on sp1.pos_id = sp2.pos_id and sp1.sal_date = sp2.MaxDate
     where new.pos_id = sp1.pos_id and
 			sal_id = 1;
-	
+/*
     if new.salary > hisal then
-		set diff = new.salary - hisal;
-		insert into EmpHistory values (new.eid, new.pos_id, now(), (new.salary - diff),
-			new.comments, new.status_id);
+		set new.salary = hisal;
 	end if;
-    
+*/
     if new.salary < lowsal then
-		set diff = lowsal - new.salary;
-		insert into EmpHistory values (new.eid, new.pos_id, now(), (new.salary + diff),
-			new.comments, new.status_id);
+		set new.salary = lowsal;
 	end if;
-    
 end;
 //
 
+delimiter |
+create trigger SalAdjHi before insert on EmpHistory
+for each row begin
+	declare hisal int;
+
+    select sal_value into hisal
+    from salpos sp1 inner join (
+    select pos_id, max(sal_date) as MaxDate
+	from SalPos
+	group by pos_id) sp2 on sp1.pos_id = sp2.pos_id and sp1.sal_date = sp2.MaxDate
+    where new.pos_id = sp1.pos_id and
+			sal_id = 2;
+
+    if new.salary > hisal then
+		set new.salary = hisal;
+	end if;
+end;
+|
